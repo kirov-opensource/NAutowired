e9c7086935fd5d32b7f99f0947503003b897a09e
Fat Tiger <wangshenjie@huidugroup.cn>
2019-04-19 13:36:55 +0800

    update nuget_pack.bat

--- scripts/nuget_pack.bat
+++ scripts/nuget_pack.bat
@@ -1,2 +1,4 @@
+dotnet build NAutowired\NAutowired.csproj -c Release
+dotnet build NAutowired.Core\NAutowired.Core.csproj -c Release
 nuget pack NAutowired\NAutowired.csproj -Prop Configuration=Release
 nuget pack NAutowired.Core\NAutowired.Core.csproj -Prop Configuration=Release
