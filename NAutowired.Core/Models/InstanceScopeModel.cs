382c41fd1a607841fa49af16675de84ebc6f5009
Fat Tiger <wangshenjie@huidugroup.cn>
2019-04-25 00:45:42 +0800

    bump to v1.0.7

---
+++ NAutowired.Core/Models/InstanceScopeModel.cs
@@ -0,0 +1,14 @@
+﻿namespace NAutowired.Core.Models {
+  public class InstanceScopeModel {
+
+    /// <summary>
+    /// 实例
+    /// </summary>
+    public object Instance { get; set; }
+
+    /// <summary>
+    /// 父级
+    /// </summary>
+    public InstanceScopeModel ParentInstanceScope { get; set; }
+  }
+}
--- NAutowired/NAutowired.cs
+++ NAutowired/NAutowired.cs
@@ -1,8 +1,7 @@
 ﻿using NAutowired.Core.Attributes;
+using NAutowired.Core.Models;
 using NAutowired.Exceptions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 
 namespace NAutowired {
@@ -15,29 +14,27 @@ namespace NAutowired {
     /// <param name="serviceProvider"></param>
     /// <param name="typeInstance"></param>
     public static void FieldDependencyInjection<T>(IServiceProvider serviceProvider, T typeInstance) {
-      AnalysisDependencyInjection(serviceProvider, typeInstance, new List<object>());
+      AnalysisDependencyInjection(serviceProvider, new InstanceScopeModel { Instance = typeInstance });
     }
 
     /// <summary>
     /// 分析
     /// </summary>
-    /// <typeparam name="T"></typeparam>
     /// <param name="serviceProvider"></param>
-    /// <param name="typeInstance"></param>
-    /// <param name="resolvedInstances"></param>
-    private static void AnalysisDependencyInjection<T>(IServiceProvider serviceProvider, T typeInstance, IList<object> resolvedInstances) {
-      foreach (var propertity in typeInstance.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)) {
+    /// <param name="instanceScopeModel"></param>
+    private static void AnalysisDependencyInjection(IServiceProvider serviceProvider, InstanceScopeModel instanceScopeModel) {
+      foreach (var propertity in instanceScopeModel.Instance.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)) {
         //判断当前属性是否具有DependencyInjectionAttribute特性
         var customeAttribute = propertity.GetCustomAttribute(typeof(AutowiredAttribute), false);
         if (customeAttribute == null) {
           continue;
         }
+
         var type = ((AutowiredAttribute)customeAttribute).RealType ?? propertity.PropertyType;
-        //从以还原集合拿到Instance
-        var value = resolvedInstances.FirstOrDefault(item => item.GetType() == type);
+        var value = GetInstance(instanceScopeModel, type);
+        //从parent instance 还原
         if (value != null) {
-          //将Instance赋值给属性
-          propertity.SetValue(typeInstance, value);
+          propertity.SetValue(instanceScopeModel.Instance, value);
           continue;
         }
         //从容器拿到Instance
@@ -45,12 +42,30 @@ namespace NAutowired {
         if (value == null) {
           throw new UnableResolveDependencyException($"Unable to resolve dependency {propertity.PropertyType.FullName}");
         }
-        resolvedInstances.Add(value);
         //将Instance赋值给属性
-        propertity.SetValue(typeInstance, value);
+        propertity.SetValue(instanceScopeModel.Instance, value);
+        //构建下一个节点
+        var nextInstanceScopeModel = new InstanceScopeModel {
+          Instance = value,
+          ParentInstanceScope = instanceScopeModel
+        };
         //递归注入的属性是否有其它依赖
-        AnalysisDependencyInjection(serviceProvider, value, resolvedInstances);
+        AnalysisDependencyInjection(serviceProvider, nextInstanceScopeModel);
       }
+    }
+
+    /// <summary>
+    /// 递归查找父节点
+    /// </summary>
+    /// <param name="instanceScopeModel"></param>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    private static object GetInstance(InstanceScopeModel instanceScopeModel, Type type) {
+      if (instanceScopeModel.Instance.GetType() == type) {
+        return instanceScopeModel.Instance;
+      }
+      if (instanceScopeModel.ParentInstanceScope == null) { return null; }
+      return GetInstance(instanceScopeModel.ParentInstanceScope, type);
     }
   }
 }
