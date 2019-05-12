96a09c7ba67a817110561da8c34ac81d07f030fd
Fat Tiger <w1752738919@gmail.com>
2019-05-12 13:08:52 +0800

    bump to v1.0.9

---
+++ NAutowired/Exceptions/AddDependencyInjectionException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NAutowired.Exceptions {
+  public class AddDependencyInjectionException : SystemException {
+
+    public AddDependencyInjectionException() {
+
+    }
+
+    public AddDependencyInjectionException(string message) : base(message) { }
+
+  }
+}
