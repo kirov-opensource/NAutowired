4f59e733e8654295a6a30be6acd5f8cce45a9a0d
Fat Tiger <w1752738919@gmail.com>
2019-09-06 18:47:28 +0800

    bump to v1.2.0

--- scripts/nuget_pack.bat
+++ scripts/nuget_pack.bat
@@ -1,4 +1,6 @@
 dotnet build NAutowired\NAutowired.csproj -c Release
 dotnet build NAutowired.Core\NAutowired.Core.csproj -c Release
+dotnet build NAutowired.Console\NAutowired.Console.csproj -c Release
 nuget pack NAutowired\NAutowired.csproj -Prop Configuration=Release
 nuget pack NAutowired.Core\NAutowired.Core.csproj -Prop Configuration=Release
+nuget pack NAutowired.Console\NAutowired.Console.csproj -Prop Configuration=Release
