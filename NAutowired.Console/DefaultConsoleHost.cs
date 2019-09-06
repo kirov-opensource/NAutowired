0fbe9ab587d616c67794f536b538fbbe09a8c2f5
Fat Tiger <w1752738919@gmail.com>
2019-09-06 18:05:44 +0800

    update nuget version

--- NAutowired.Console/DefaultConsoleHost.cs
+++ NAutowired.Console/DefaultConsoleHost.cs
@@ -1,25 +1,44 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using NAutowired.Core;
+using System;
 
 namespace NAutowired.Console
 {
     public class DefaultConsoleHost : IConsoleHost
     {
         private string[] args;
-        private IServiceCollection serviceCollection;
+        private IServiceProvider serviceProvider;
 
         public DefaultConsoleHost(IServiceCollection serviceCollection, string[] args)
         {
-            this.serviceCollection = serviceCollection;
             this.args = args;
+            this.serviceProvider = serviceCollection.BuildServiceProvider();
         }
 
 
         public void Run<TStartup>() where TStartup : Startup, new()
         {
             var instance = new TStartup();
-            DependencyInjection.Resolve(serviceCollection.BuildServiceProvider(), instance);
+            DependencyInjection.Resolve(serviceProvider, instance);
             instance.Run(args);
+        }
+
+        public T GetService<T>()
+        {
+            var instance = serviceProvider.GetService<T>();
+            if (instance == null)
+                return default;
+            DependencyInjection.Resolve(serviceProvider, instance);
+            return instance;
+        }
+
+        public object GetService(Type type)
+        {
+            var instance = serviceProvider.GetService(type);
+            if (instance == null)
+                return default;
+            DependencyInjection.Resolve(serviceProvider, instance);
+            return instance;
         }
     }
 }
