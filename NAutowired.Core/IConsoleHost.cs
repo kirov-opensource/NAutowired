0b61a70ef14ff3a53697971311c430c0838714f0
Fat Tiger <w1752738919@gmail.com>
2019-09-09 13:32:11 +0800

    bump to v1.2-alpha.2

--- NAutowired.Core/IConsoleHost.cs
+++ NAutowired.Core/IConsoleHost.cs
@@ -1,7 +1,13 @@
-﻿namespace NAutowired.Core
+﻿using System;
+
+namespace NAutowired.Core
 {
     public interface IConsoleHost
     {
         void Run<TStartup>() where TStartup : Startup, new();
+
+        T GetService<T>();
+
+        object GetService(Type type);
     }
 }
