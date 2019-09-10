643715f31132b8e19d58b6b722a483bf971efead
FatTiger <wangshenjie@colipu.com>
2019-09-10 10:10:42 +0800

    support duck typing

--- NAutowired.Core/DependencyInjection.cs
+++ NAutowired.Core/DependencyInjection.cs
@@ -5,70 +5,81 @@ using NAutowired.Core.Models;
 using System;
 using System.Reflection;
 
-namespace NAutowired.Core {
-  public static class DependencyInjection {
+namespace NAutowired.Core
+{
+    public static class DependencyInjection
+    {
+        private readonly static Type autowiredAttributeType = typeof(AutowiredAttribute);
 
-    private readonly static Type autowiredAttributeType = typeof(AutowiredAttribute);
-
-    /// <summary>
-    /// 属性依赖注入
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="serviceProvider"></param>
-    /// <param name="typeInstance"></param>
-    public static void Resolve<T>(IServiceProvider serviceProvider, T typeInstance) {
-      ResolveDependencyTree(serviceProvider, new InstanceScopeModel { Instance = typeInstance });
-    }
-
-    /// <summary>
-    /// 分析
-    /// </summary>
-    /// <param name="serviceProvider"></param>
-    /// <param name="instanceScopeModel"></param>
-    private static void ResolveDependencyTree(IServiceProvider serviceProvider, InstanceScopeModel instanceScopeModel) {
-      foreach (var fieldInfo in instanceScopeModel.Instance.GetType().GetFullFields()) {
-        //判断当前属性是否具有DependencyInjectionAttribute特性
-        var customeAttribute = fieldInfo.GetCustomAttribute(autowiredAttributeType, false);
-        if (customeAttribute == null) {
-          continue;
+        /// <summary>
+        /// 属性依赖注入
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="serviceProvider"></param>
+        /// <param name="typeInstance"></param>
+        public static void Resolve<T>(IServiceProvider serviceProvider, T typeInstance)
+        {
+            ResolveDependencyTree(serviceProvider, new InstanceScopeModel { Instance = typeInstance });
+            typeof(T).InvokeConstructor(typeInstance);
         }
 
-        var type = ((AutowiredAttribute)customeAttribute).RealType ?? fieldInfo.FieldType;
-        var value = GetInstance(instanceScopeModel, type);
-        //从parent instance 还原
-        if (value != null) {
-          fieldInfo.SetValue(instanceScopeModel.Instance, value);
-          continue;
-        }
-        //从容器拿到Instance
-        value = serviceProvider.GetService(type);
-        if (value == null) {
-          throw new UnableResolveDependencyException($"Unable to resolve dependency {fieldInfo.FieldType.FullName}");
-        }
-        //将Instance赋值给属性
-        fieldInfo.SetValue(instanceScopeModel.Instance, value);
-        //构建下一个节点
-        var nextInstanceScopeModel = new InstanceScopeModel {
-          Instance = value,
-          ParentInstanceScope = instanceScopeModel
-        };
-        //递归注入的属性是否有其它依赖
-        ResolveDependencyTree(serviceProvider, nextInstanceScopeModel);
-      }
-    }
+        /// <summary>
+        /// 分析
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <param name="instanceScopeModel"></param>
+        private static void ResolveDependencyTree(IServiceProvider serviceProvider, InstanceScopeModel instanceScopeModel)
+        {
+            foreach (var fieldInfo in instanceScopeModel.Instance.GetType().GetFullFields())
+            {
+                //判断当前属性是否具有DependencyInjectionAttribute特性
+                var customeAttribute = fieldInfo.GetCustomAttribute(autowiredAttributeType, false);
+                if (customeAttribute == null)
+                {
+                    continue;
+                }
 
-    /// <summary>
-    /// 递归查找父节点
-    /// </summary>
-    /// <param name="instanceScopeModel"></param>
-    /// <param name="type"></param>
-    /// <returns></returns>
-    private static object GetInstance(InstanceScopeModel instanceScopeModel, Type type) {
-      if (instanceScopeModel.Instance.GetType() == type) {
-        return instanceScopeModel.Instance;
-      }
-      if (instanceScopeModel.ParentInstanceScope == null) { return null; }
-      return GetInstance(instanceScopeModel.ParentInstanceScope, type);
+                var type = ((AutowiredAttribute)customeAttribute).RealType ?? fieldInfo.FieldType;
+                var value = GetInstance(instanceScopeModel, type);
+                //从parent instance 还原
+                if (value != null)
+                {
+                    fieldInfo.SetValue(instanceScopeModel.Instance, value);
+                    continue;
+                }
+                //从容器拿到Instance
+                value = serviceProvider.GetService(type);
+                if (value == null)
+                {
+                    throw new UnableResolveDependencyException($"Unable to resolve dependency {fieldInfo.FieldType.FullName}");
+                }
+                //将Instance赋值给属性
+                fieldInfo.SetValue(instanceScopeModel.Instance, value);
+                //构建下一个节点
+                var nextInstanceScopeModel = new InstanceScopeModel
+                {
+                    Instance = value,
+                    ParentInstanceScope = instanceScopeModel
+                };
+                //递归注入的属性是否有其它依赖
+                ResolveDependencyTree(serviceProvider, nextInstanceScopeModel);
+            }
+        }
+
+        /// <summary>
+        /// 递归查找父节点
+        /// </summary>
+        /// <param name="instanceScopeModel"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private static object GetInstance(InstanceScopeModel instanceScopeModel, Type type)
+        {
+            if (instanceScopeModel.Instance.GetType() == type)
+            {
+                return instanceScopeModel.Instance;
+            }
+            if (instanceScopeModel.ParentInstanceScope == null) { return null; }
+            return GetInstance(instanceScopeModel.ParentInstanceScope, type);
+        }
     }
-  }
 }
--- NAutowired.Core/Extensions/TypeExtension.cs
+++ NAutowired.Core/Extensions/TypeExtension.cs
@@ -2,22 +2,41 @@
 using System.Collections.Generic;
 using System.Reflection;
 
-namespace NAutowired.Core.Extensions {
-  internal static class TypeExtension {
+namespace NAutowired.Core.Extensions
+{
+    internal static class TypeExtension
+    {
 
-    private readonly static Type baseType = typeof(object);
+        private readonly static Type baseType = typeof(object);
 
-    internal static IList<FieldInfo> GetFullFields(this Type type) {
-      return GetFullFields(type, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly, new List<FieldInfo>());
+        internal static IList<FieldInfo> GetFullFields(this Type type)
+        {
+            return GetFullFields(type, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly, new List<FieldInfo>());
+        }
+
+        private static IList<FieldInfo> GetFullFields(this Type type, BindingFlags bindingFlags, List<FieldInfo> fieldInfos)
+        {
+            fieldInfos.AddRange(type.GetFields(bindingFlags));
+            if (type.BaseType == baseType)
+            {
+                return fieldInfos;
+            }
+            return GetFullFields(type.BaseType, bindingFlags, fieldInfos);
+        }
+
+        internal static void InvokeConstructor(this Type type, object instance)
+        {
+            if (type.BaseType != baseType)
+            {
+                InvokeConstructor(type.BaseType, instance);
+            }
+            var methodInfo = type.GetMethod("Constructor", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly | BindingFlags.IgnoreCase, null, new Type[] { }, null);
+            if (methodInfo == null)
+            {
+                return;
+            }
+            methodInfo.Invoke(instance, null);
+        }
+
     }
-
-    private static IList<FieldInfo> GetFullFields(this Type type, BindingFlags bindingFlags, List<FieldInfo> fieldInfos) {
-      fieldInfos.AddRange(type.GetFields(bindingFlags));
-      if (type.BaseType == baseType) {
-        return fieldInfos;
-      }
-      return GetFullFields(type.BaseType, bindingFlags, fieldInfos);
-    }
-
-  }
 }
